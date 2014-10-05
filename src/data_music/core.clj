6f74df3357ae9f886897f5f4cdfe0593c90bee48
Dan Calacci <dan@dcalacci.net>
2014-10-04 23:51:14 -0400

    some good examples

--- src/data_music/core.clj
+++ src/data_music/core.clj
@@ -1,6 +1,7 @@
 (ns data-music.core)
 
-(use 'overtone.core)
+(use '[overtone.core]
+     '[overtone.inst.sampled-piano])
 (boot-server)
 
 (require '[clojure.data.csv :as csv]
@@ -9,20 +10,27 @@
          '[overtone.inst synth]
          '[data-music.instruments])
 
+;; don't require using the namespace before functions!
 (refer 'data-music.instruments)
 
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
-;; (definst sin-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
-;;     (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
-;;        (sin-osc freq)
-;;        vol))
 
 (defn -main
   [& args]
   (sin-wave)
   (noisey))
+
+
+(defn saw-beat [start] (for [i (range  start (+ start 20))] (at (+ (now) (* i 20)) (saw-wave i))))
+(saw-beat)
+
+;; play a chord progression on our piano
+(let [time (now)]
+  (at time (saw-beat 100))
+  (at (+ 1000000 time) (saw-beat 120))
+  (at (+ 20000000 time) (saw-beat 140)))
+
+(let [time (now)]
+  (at time (play-chord (chord :D3 :major7)))
+  (at (+ 2000 time) (play-chord (chord :A3 :major)))
+  (at (+ 3000 time) (play-chord (chord :A3 :major7)))
+  (at (+ 4300 time) (play-chord (chord :F3 :major7))))
--- src/data_music/instruments.clj
+++ src/data_music/instruments.clj
@@ -1,6 +1,8 @@
 (ns data-music.instruments)
 
-(use 'overtone.core)
+(use '[overtone.core]
+     '[overtone.inst.drum])
+
 
 (definst saw-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
   (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
@@ -26,3 +28,47 @@
     (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
        (sin-osc freq)
        vol))
+
+(saw-wave)
+
+(def sample-data [125 193 182 62])
+
+;; plays given frequencies with the given data, separator, and function
+(defn play-notes [data & {:keys [sep fn] :or {sep 500 fn saw-wave}}]
+  (for [i data]
+    (do (fn i)
+        (Thread/sleep sep))))
+
+
+;; this is good
+(defn play-notes [freqs & {:keys [time sep fn] :or {time (now) sep 500 fn saw-wave}}]
+  (let [freq (first freqs)]
+    (when freq
+      (at time (fn freq)))
+    (let [next-time (+ time sep)]
+      (apply-by next-time play-notes [(rest freqs) :sep sep :fn fn]))))
+
+;; works okay
+(do (play-notes sample-data :sep 500 :fn triangle-wave)
+    (play-notes sample-data :sep 500 :fn saw-wave))
+
+(kick)
+
+
+(def one-twenty-bpm (metronome 120))
+; this function will play our sound at whatever tempo we've set our metronome to 
+(defn looper [nome sound]
+    (let [beat (nome)]
+        (at (nome beat) (sound))
+        (apply-by (nome (inc beat)) looper nome sound [])))
+
+(do
+  (looper (metronome 70) saw-wave)
+  (looper (metronome 60) saw-wave)
+  (looper (metronome 120) hat3))
+
+(stop)
+
+(type kick)
+
+(def sw (saw-wave 80))
